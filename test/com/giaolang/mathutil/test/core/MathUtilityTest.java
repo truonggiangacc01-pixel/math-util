1b906e0a297d28fe2e7bff6b2f4d480dbb45cc9d
truonggiangacc01-pixel <truonggiangacc01@gmail.com>
2026-07-26 23:09:08 +0700

    update code to see green 4ever

--- test/com/giaolang/mathutil/test/core/MathUtilityTest.java
+++ test/com/giaolang/mathutil/test/core/MathUtilityTest.java
@@ -38,7 +38,7 @@ public class MathUtilityTest {
         Assert.assertEquals(6, MathUtil.getFactorial(3)); //tui muốn 3! == 6
         Assert.assertEquals(24, MathUtil.getFactorial(4)); //tui muốn 4! == 24
         Assert.assertEquals(120, MathUtil.getFactorial(5)); //tui muốn 5! == 120
-        Assert.assertEquals(120, MathUtil.getFactorial(6)); //tui muốn 6! == 720
+        Assert.assertEquals(720, MathUtil.getFactorial(6)); //tui muốn 6! == 720
 
         //hàm giúp so sánh 2 giá trị nào đó có giống nhau hay không 
         //nếu giống nhau -> thảy màu xanh đèn đường - đường thông, code ngon
