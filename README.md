96f5bd8bf7863143bb8d76637897f873e6c079fe
truonggiangacc01-pixel <truonggiangacc01@gmail.com>
2026-07-28 17:43:55 +0700

    refactor the main code and green 4ever alwayts

--- README.md
+++ README.md
@@ -4,9 +4,13 @@
 ### You will find in this repo the following stuff:
 * The Math-Util library that offers multiple math functions (a Java-based source code)
 * Unit Testing source code with JUnit framework
+* GitHub Actions to demonstrate the Continuous Integration (CI) technique
+* Regression Test 
 * ...
 ###Screenshots
 ![DDT & TDD with JUnit](https://github.com/truonggiangacc01-pixel/math-util/blob/main/images/DDT%20with%20Junit.png)
+
+
 
 
 ### @2026 truonggiang.ac01
--- src/com/giaolang/mathutil/core/MathUtil.java
+++ src/com/giaolang/mathutil/core/MathUtil.java
@@ -20,19 +20,48 @@ public class MathUtil {
     //20 giai thừa thừa 18 con số 0, vừa kịp đủ cho kiểu int 
     //21 giai thừa tràn kiểu long
     //bài này quy ước tính n! trong khoảng từ 0.200
+//    public static long getFactorial(int n){
+//        if(n < 0 || n > 20)
+//            throw new IllegalArgumentException
+//            ("Invalid argument. N must be between 0..20");
+//        
+//        if(n == 0 || n == 1)
+//            return 1; //kết thúc cuộc chơi sớm nếu nhận những đầu vào đặc biệt
+//        
+//        long product = 1; //tích nhân dồn, thuật toán con heo đất, ốc bu dồn thịt
+//        
+//        for (int i = 2; i <= n; i++) 
+//            product *= i;
+//        return product;
+//    }
+    
+    //Học đệ quy trong vòng 30s - RECUSION
+    //Hiện tượng gọi lại chính mình với 1 quy mô khác
+    //Ví dụ: con búp bê Nga, giống nhau và lồng trong nhau
+    //búp bê to, nhỏ hơn, nhỏ hơn nữa, ... đến điểm dừng
+    
+    //tính giùm tui 6!
+    //n! = 1.2.3.4.5.6 .... n 
+    //6! = 6 x 5!
+    //5! = 5 x 4! 
+    //4! = 4 x 3! 
+    //3! = 3 x 2!
+    //2! = 2 x 1! //điềm dừng
+    //1! = 1
+    
+    //chốt hạ: n! = n * (n - 1)!
+    
+    //quy ước 1! = 0! = 1
+    
     public static long getFactorial(int n){
-        if(n < 0 || n > 20)
+        
+         if(n < 0 || n > 20)
             throw new IllegalArgumentException
             ("Invalid argument. N must be between 0..20");
         
         if(n == 0 || n == 1)
             return 1; //kết thúc cuộc chơi sớm nếu nhận những đầu vào đặc biệt
         
-        long product = 1; //tích nhân dồn, thuật toán con heo đất, ốc bu dồn thịt
-        
-        for (int i = 2; i <= n; i++) 
-            product *= i;
-        return product;
+        return n * getFactorial(n - 1); //công thức đệ quy
     }
-    
 }
